1ef30505ce5dee0c70708edb6b4b1771a0622a6c
Yusufsy <yusufsunusi63@gmail.com>
2023-10-02 23:36:08 +0800

    Cors setup

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'https://bjmls-console.web.app', // Replace with your allowed origin(s)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and credentials
+};
+
+app.use(cors(corsOptions));
 
 // Use the routes from routes.js
 app.use('/', routes);
